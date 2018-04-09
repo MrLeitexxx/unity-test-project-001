1f223e93d4ce7f54708f2169851f299868f8df64
MrLeitexxx <lei_gauci@hotmail.com>
2018-04-09 23:19:10 +0200

    Initial Commit

---
+++ Test project 001/Assets/LevelOnePlatformMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOnePlatformMove : MonoBehaviour {
+
+	private Vector2 direction = Vector2.right;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (direction * 0.03f);
+
+		if (transform.position.x < 21.0f) {
+
+			direction = Vector2.right;
+		}
+
+		if (transform.position.x > 27.0f) {
+
+			direction = Vector2.left;
+		}
+
+	}
+}
---
+++ Test project 001/Assets/WalkingEnemyAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingEnemyAI : MonoBehaviour {
+
+	private int enemySpeed = 2;
+	private int xMoveDirection = -1;
+	//private bool facingRight = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (xMoveDirection, 0));
+//
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (xMoveDirection, 0) * enemySpeed;
+//
+//		if (hit.distance < 2.7f && hit.collider.tag == "Obstacle" ) {
+//
+//			Flip ();
+//			Debug.Log ("Monster has collided with " + hit.collider.tag);
+//		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		Debug.Log ("Monster has collided with " + col.collider.tag);
+
+		if (col.collider.tag == "Obstacle" || col.collider.tag == "Enemy") {
+
+			Flip ();
+		}
+
+	}
+
+	void Flip(){
+
+		Vector2 localScale = gameObject.transform.localScale;
+		localScale.x *= -1;
+		transform.localScale = localScale;
+		xMoveDirection *= -1;
+	}
+}
